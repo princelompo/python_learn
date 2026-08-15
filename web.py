764b6ff4884fa72d1383e02eb06c0bd743f71fe7
Yemboado Lompo <yems97141@gmail.com>
2026-08-15 09:44:46 +0000

    mise à jour de web.py

--- web.py
+++ web.py
@@ -5,5 +5,6 @@ def ouvrir_site(url):
     webbrowser.open_new_tab(url)
 print("\n✨OUVRIR UN SITE WEB AVEC LE MODULE WEBBROWSER✨\n")
 url = input("Entrez l'URL du site à ouvrir (ex: https://www.python.org) : ")
+print(f"Ouverture du site : {url}")
 ouvrir_site(url)
 
